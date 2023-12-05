cc08e619fa9a0236c7a21f6e7ea2566a4a6b090b
fancyzhong <fancyzhong@tencent.com>
2023-12-06 00:51:43 +0800

    feat: add msg

--- test/api/immut.ts
+++ test/api/immut.ts
@@ -3,6 +3,7 @@ import { dictFictory } from '../_data';
 
 describe('check onOperate', () => {
 
+  // @see 3.11.1 commit msg: https://github.com/tnfe/limu/commit/98b7c3ed2834b3c7ed0c3ceefcd7d6afb73d9329
   test('onOperate: immut map', () => {
     const base = dictFictory();
     const draft = immut(base);
